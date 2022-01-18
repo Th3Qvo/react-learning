7260e58fda1ae8a7a4f0889d6dab45fa0f7367de
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2022-01-18 15:27:41 +0100

    ADD Provider & store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 /** znajdź element, którego id to 'app', i wstaw do niego komponent <App /> */
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
 
