2f089af2f1f6ebf8be7b07056b781db8f3d27a54
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2022-02-12 20:42:45 +0100

    ADD FAQ.js

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Container from '../Container/Container';
+import Hero from '../Hero/Hero';
 
 const FAQ = () => (
   <Container>
-    <h2>Frequently Asked Questions</h2>
+    <Hero titleText='FAQ' imageBGI='https://images.unsplash.com/photo-1597484661973-ee6cd0b6482c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3348&q=80' />
     <p><strong>Question 1</strong></p>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     <p><strong>Question 2</strong></p>
