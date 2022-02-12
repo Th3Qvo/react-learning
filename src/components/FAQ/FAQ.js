797468002ba0e8cdfc3a6f2ea87a4ce076927713
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2022-02-12 20:17:28 +0100

    ADD component FAQ

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from '../Container/Container';
+
+const FAQ = () => (
+  <Container>
+    <h2>Frequently Asked Questions</h2>
+    <p><strong>Question 1</strong></p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <p><strong>Question 2</strong></p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <p><strong>Question 3</strong></p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <p><strong>Question 4</strong></p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <p><strong>Question 5</strong></p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </Container>
+);
+
+export default FAQ;
