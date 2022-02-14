9d04d18ae4bfba004fc5c9a54995e9a2ac0f3b49
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2022-02-14 16:59:05 +0100

    UPDATE Search, cardsRedux, store

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,11 +1,9 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForColumn = ({ cards, searchString }, columnId) => {
-  return cards.filter(
-    card => card.columnId == columnId && new RegExp(searchString, 'i').test(card.title)
-  );
-};
+export const getCardsForColumn = ({ cards }, columnId) => cards.filter((card) => card.columnId == columnId);
+
+export const getCardsForSearchResults = ({ cards }, cardId) => cards.filter((card) => new RegExp(cardId, 'i').test(card.title));
 
 // action name creator
 const reducerName = 'cards';
