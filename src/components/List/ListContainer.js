8033b2f4f57ea10dea98c1ab489c8fca8b8b6b55
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2022-02-14 16:28:26 +0100

    UPDATE ListContainer

--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -9,7 +9,7 @@ const mapStateToProps = (state, props) => {
 
   return {
     ...listParams,
-    columns: getColumnsForList(state, props.id),
+    columns: getColumnsForList(state, id),
   };
 };
 
