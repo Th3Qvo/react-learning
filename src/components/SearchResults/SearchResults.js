a1e88e3a3ba62d4ff05bc5ef8c4c89d3337b963d
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2022-02-14 18:18:39 +0100

    ADD SearchResults

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './SearchResults.scss';
+import Card from '../Card/Card';
+import { settings } from '../../data/dataStore';
+
+class SearchResults extends React.Component {
+
+  static propTypes = {
+    cards: PropTypes.array,
+  };
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  };
+
+  render() {
+    const { cards } = this.props;
+
+    return (
+      <section className={styles.component}>
+
+        <div className={styles.cards}>
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default SearchResults;
