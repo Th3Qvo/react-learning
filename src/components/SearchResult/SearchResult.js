0cbb3a874dfdb100ae88016ad7caf351d58df3a2
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2022-02-14 17:15:00 +0100

    UPDATE SearchResult

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,36 @@
+/** w kazdym pliku react trzeba importować bibliotekę react */
+import React from 'react';
+import styles from './SearchResult.scss';
+import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import { settings } from '../../data/dataStore';
+import Icon from '../Icon/Icon';
+
+
+class Column extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.node,
+    cards: PropTypes.array,
+  };
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  };
+
+  render() {
+    const { title, icon, cards } = this.props;
+    return (
+      <section className={styles.component}>
+        <h3 className={styles.title}> <span className={styles.icon}><Icon name={icon} /></span>{title} </h3>
+        <div>
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Column;
