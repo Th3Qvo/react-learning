809748b5e6279a6786ffb7acdcb5b5f489fa41f8
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2022-02-21 15:21:01 +0100

    BUG FIX

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -38,7 +38,7 @@ class Search extends React.Component {
           onChange={event => this.handleChange(event)}
         />
         <div className={styles.buttons}>
-          <Button onClick={this.handleOK}><Icon name={icon} /></Button>
+          <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
         </div>
       </div>
     );
--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -4,8 +4,11 @@ import { getCardsForSearchResults } from '../../redux/cardsRedux';
 
 const mapStateToProps = (state, props) => {
   const cardId = props.match.params.cardId;
+  const fliteredCards = state.cards.filter(card => card.id == cardId);
+  const cardsParams = fliteredCards[0] || {};
 
   return {
+    ...cardsParams,
     cards: getCardsForSearchResults(state, cardId),
   };
 };
